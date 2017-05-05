6e72e842104147a07406cb0ce0f03f32da73f11b
Lucas Hawes <doubletigerstripe@gmail.com>
2017-05-04 22:52:13 -0700

    Setup : Creates the build direcotry for the react app.

---
+++ build/reactapp/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'React app working'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  };
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
