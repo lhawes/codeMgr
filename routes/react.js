79247ea3f8d50aca348f62533800baa9ecf2ce6b
Lucas Hawes <doubletigerstripe@gmail.com>
2017-05-05 21:25:00 -0700

    Feat : Added test object to pass to jade template.

--- routes/react.js
+++ routes/react.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var info = {
+  title: 'react',
+  testObj: JSON.stringify({prop: 'test', num: 12})
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('react', { title: 'react' });
+  res.render('react', info);
 });
 
 module.exports = router;
